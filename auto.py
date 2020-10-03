cb32db52b9ec8d25902afc787b610125660347fa
Paramesh Chandra <51922459+paramphy@users.noreply.github.com>
2020-10-03 16:55:21 +0530

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,18 +1,34 @@
 import pyautogui
 import time 
 
-x = 800
-y = 500
-while True:
-    pyautogui.moveTo(100, 100, duration = 1)
-    pyautogui.scroll(200) 
-    pyautogui.moveTo(100 + x, 100, duration = 1)
-    pyautogui.scroll(200) 
-    pyautogui.moveTo(100 + x, 100 + y, duration = 1)
-    pyautogui.scroll(200) 
-    pyautogui.moveTo(100, 100 + y, duration = 1)
-    pyautogui.scroll(200)
-    time.sleep(20)  
+def ghost():
+    x = 800
+    y = 500
+    while True:
+        pyautogui.moveTo(100, 100, duration = 1)
+        pyautogui.scroll(200) 
+        pyautogui.moveTo(100 + x, 100, duration = 1)
+        pyautogui.scroll(200) 
+        pyautogui.moveTo(100 + x, 100 + y, duration = 1)
+        pyautogui.scroll(200) 
+        pyautogui.moveTo(100, 100 + y, duration = 1)
+        pyautogui.scroll(200)
+        time.sleep(20)
+        #return("It is Gosting") 
 
-  
-
+#def send_mail():
+#posfile = open('pos.txt','w')
+#x = []
+#y = []
+#while True:
+#    pos = pyautogui.position()
+#    posfile.write(str(pos[0]))
+#    posfile.write(",")
+#    posfile.write(str(pos[1]))
+#    posfile.write("\n")
+#    time.sleep(1)
+with open("pos.txt", "r") as file: 
+    data = file.readlines() 
+    for line in data: 
+        word = line.split(',') 
+        print(word) 
