a3732d1da693dea0e478ff19f1ed6f35e097857d
Paramesh Chandra <51922459+paramphy@users.noreply.github.com>
2020-10-17 21:12:49 +0530

    Update autoscroll.py

--- autoscroll.py
+++ autoscroll.py
@@ -6,11 +6,12 @@ init_pos = [pos.x,pos.y]
 
 while True:
     pos = pyautogui.position()
-    print(pos.x)
+    #print(pos.x)
     final_pos = [pos.x,pos.y]
     if final_pos[0] == init_pos[0] & final_pos[1] == init_pos[1]:
         pyautogui.scroll(200)
         for i in range(0,1):
             init_pos[i] = final_pos[i]
+        print(final_pos)
     time.sleep(1)
     
